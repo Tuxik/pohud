f66224285d1dbd143587d6fcdf5e24977cb5779c
Johann LAHITTE <jlahitte@bricodepot.com>
2015-06-03 17:15:20 +0200

    Mise en place des tests unitaires

--- src/main/java/com/po/pohud.java
+++ src/main/java/com/po/pohud.java
@@ -1,16 +1,9 @@
 package com.po;
 
-import com.po.file.FileReaderWMX;
-import com.po.file.FileUtils;
 
 public class pohud {
 
 	public static void main(String[] args) {
-		String workingDir = System.getProperty("user.dir");
-		String s = FileUtils.getContentOfFile(workingDir + "/test-res/WHistory.txt");
-		System.out.println(FileReaderWMX.getNumberOfHands(s));
-		for (String sHid : FileReaderWMX.getHandsId(s)) {
-			System.out.println(sHid);
-		}
+		// TODO
 	}
 }
